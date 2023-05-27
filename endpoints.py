c642a6ed90eeb054015f4ffc12aa0251059295eb
Thiago Manzi <thiago_manzi@hotmail.com>
2023-05-27 02:20:14 -0300

    fix: arrumado erro no corpo da requisicao ao barramento desserializando o evento e implementado regex para limpeza de respostas da OpenAI

--- endpoints.py
+++ endpoints.py
@@ -1,31 +1,33 @@
 from fastapi import FastAPI
-from ChatGPTRequestBody import ChatGPTRequestBody
 from Evento import Evento
 import requests
 import chatgpt
+import re
 
 
 def criar_endpoints(OPENAI_API_KEY):
     app = FastAPI()
 
-    @app.get("/")
-    async def root():
-        return {"message": "Hello, FastAPI"}
-
-    @app.post("/sentimentos")
-    async def root(chatGPTRequestBody: ChatGPTRequestBody):
-        return chatgpt.encontrar_sentimento(OPENAI_API_KEY, chatGPTRequestBody)
-
     @app.post("/eventos")
     async def root(evento: Evento):
         def LembreteCriado(evento: Evento):
             evento.tipo = "LembreteAnalisado"
-            evento.dados.sentimento = chatgpt.encontrar_sentimento(OPENAI_API_KEY, evento.dados.texto)
-            requests.post("http://localhost:10000/eventos", json=dict(evento))
+            while True: #enquanto o chatgpt nao entregar uma resposta válida
+                sentimento = chatgpt.encontrar_sentimento(OPENAI_API_KEY, evento.dados.texto)
+                sentimento = re.sub(r"\s"," ",sentimento) #remover enters
+                sentimento = re.sub(r"[^A-Za-z]"," ",sentimento) #remover qualquer pontuacao
+                sentimento = re.sub(r"(?i)\b(?!Positivo\b|Neutro\b|Negativo\b)\w+\b","",sentimento).strip() #remover qualquer palavra que nao seja de interesse e espaços em branco
+                if sentimento != "":
+                    break
+            evento.dados.sentimento = sentimento
+            requests.post("http://localhost:10000/eventos", data=evento.json(), headers={"Content-type": "application/json"})
             
         funcoes = {"LembreteCriado": LembreteCriado}
+
         try:
             funcoes[evento.tipo](evento)
         except:
             pass
+        return {"msg": "ok"}
+
     return app
