fd8d6a2aac1dce327c045ac12c9b5f3d62718956
Thiago Manzi <thiago_manzi@hotmail.com>
2023-05-31 17:32:07 -0300

    feat: versao 1.0, features de sentimento de observacoes e lembretes implementadas

--- DadosLembrete.py
+++ DadosLembrete.py
@@ -3,4 +3,5 @@ from pydantic import BaseModel
 class DadosLembrete(BaseModel):
     id: int
     texto: str
+    status: str
     sentimento: str
--- endpoints.py
+++ endpoints.py
@@ -10,7 +10,7 @@ def criar_endpoints(OPENAI_API_KEY):
 
     @app.post("/eventos")
     async def root(evento: Evento):
-        def LembreteCriado(evento: Evento):
+        def LembreteClassificado(evento: Evento):
             evento.tipo = "LembreteAnalisado"
             while True: #enquanto o chatgpt nao entregar uma resposta válida
                 sentimento = chatgpt.encontrar_sentimento(OPENAI_API_KEY, evento.dados.texto)
@@ -21,8 +21,20 @@ def criar_endpoints(OPENAI_API_KEY):
                     break
             evento.dados.sentimento = sentimento
             requests.post("http://localhost:10000/eventos", data=evento.json(), headers={"Content-type": "application/json"})
+
+        def ObservacaoClassificada(evento: Evento):
+            evento.tipo = "ObservacaoAnalisada"
+            while True: #enquanto o chatgpt nao entregar uma resposta válida
+                sentimento = chatgpt.encontrar_sentimento(OPENAI_API_KEY, evento.dados.texto)
+                sentimento = re.sub(r"\s"," ",sentimento) #remover enters
+                sentimento = re.sub(r"[^A-Za-z]"," ",sentimento) #remover qualquer pontuacao
+                sentimento = re.sub(r"(?i)\b(?!Positivo\b|Neutro\b|Negativo\b)\w+\b","",sentimento).strip() #remover qualquer palavra que nao seja de interesse e espaços em branco
+                if sentimento != "":
+                    break
+            evento.dados.sentimento = sentimento
+            requests.post("http://localhost:10000/eventos", data=evento.json(), headers={"Content-type": "application/json"})
             
-        funcoes = {"LembreteCriado": LembreteCriado}
+        funcoes = {"LembreteClassificado": LembreteClassificado, "ObservacaoClassificada": ObservacaoClassificada}
 
         try:
             funcoes[evento.tipo](evento)
